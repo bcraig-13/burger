c2f8051b22a5a02f80df3777c41e8b3757cd9098
Brandon Craig <thegamingraven13@gmail.com>
2021-01-23 19:09:46 -0800

    Initial commit. Created starting files, schema, and seeds. Next step in gitlab instructions is commented on server.js

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers VALUES ("Mondo Burger");
+INSERT INTO burgers VALUES ("Good Burger");
+INSERT INTO burgers VALUES ("Krabby Patty");
---
+++ server.js
@@ -0,0 +1,3 @@
+const express = require("express");
+
+//4.Run the schema.sql and seeds.sql files into the mysql server from the command line
