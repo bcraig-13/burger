01c06bcae4cabb9f822e9bf56079ca859b02283b
Brandon Craig <thegamingraven13@gmail.com>
2021-01-25 21:59:28 -0800

    Linked jQuery. Wrote script.js in the public folder.

---
+++ public/assets/js/script.js
@@ -0,0 +1,30 @@
+$(() => {
+  $(".create-form").on("submit", (event) => {
+    event.preventDefault();
+
+    const newBurger = {
+      name: $("burg-name").val().trim(),
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(() => {
+        location.reload();
+    })
+  });
+
+  $("devour").on("click", function() {
+    const id = $(this).data("id");
+    const eaten = $(this).data("eaten");
+    const inBelly = { value: eaten};
+
+    $.ajax(`/api/burgers/${id}/devoured`, {
+        type: "PUT",
+        data: JSON.stringify(inBelly),
+        contentType: "application/json; charset=UTF-8",
+    }).then(() => {
+        location.reload();
+    })
+  })
+});
